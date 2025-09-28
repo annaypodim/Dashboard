d527cfbaa41ff18acab368d927708cdb1977144a
VishalRamamurthy <165613285+VishalRamamurthy@users.noreply.github.com>
2025-09-27 17:22:00 -0700

    Revert "Update uploader.py"

--- pages/uploader.py
+++ pages/uploader.py
@@ -60,7 +60,7 @@ submit = st.button("submit")
 if submit and uploaded:
     st.write(f"CSV is reporting data for: {df['Date'].iloc[0].date()}")
     string = info_df[info_df['Name'] == year_selector]['Registration end date'].iloc[0]
-    if today >= pd.Timestamp(df['Date'].iloc[0]).date() and today <= race_end + timedelta(days=2):
+    if today >= pd.Timestamp(df['Date'].iloc[0]).date() and today <= pd.Timestamp(string).date():
         st.write("✅ data within range of race")
         if pd.Timestamp(df['Date'].iloc[-1]).date() >= pd.Timestamp(ogRace['Date'].iloc[-1]).date():
             st.write("✅ Data is okay to be written and is being written")
