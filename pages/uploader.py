d11110262e1653e25f9ae4edac73769a539e186a
VishalRamamurthy <165613285+VishalRamamurthy@users.noreply.github.com>
2025-09-27 18:34:29 -0700

    Update uploader.py

--- pages/uploader.py
+++ pages/uploader.py
@@ -56,14 +56,14 @@ if uploaded_file is not None:
 
 submit = st.button("submit")
 
-print("today": today)
-print("year_selector": year_selector)
+print("today: ",  today)
+print("year_selector: ", year_selector)
 
 if submit and uploaded:
     st.write(f"CSV is reporting data for: {df['Date'].iloc[0].date()}")
 
     string = info_df[info_df['Name'] == year_selector]['Registration end date'].iloc[0]
-    print("final date": pd.Timestamp(string).date())
+    print("final date: ", pd.Timestamp(string).date())
     if today >= pd.Timestamp(df['Date'].iloc[0]).date() and today <= pd.Timestamp(string).date():
         st.write("✅ data within range of race")
         if pd.Timestamp(df['Date'].iloc[-1]).date() >= pd.Timestamp(ogRace['Date'].iloc[-1]).date():
